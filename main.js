27cc9dbef161baaf88e1e69b3a217fb6866259ee
Riina Häidkind <riina.haidkind@khk.ee>
2018-10-10 12:27:11 +0300

    Lisatud naelade ja untside kalkuleerimise võimalus

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 let kgInput = document.getElementById('kgInput');
 let gramOutput = document.getElementById('gramOutput');
+let poundOutput = document.getElementById('poundOutput');
+let ozOutput = document.getElementById('ozOutput');
 // Listening to events
 kgInput.addEventListener('input', event => {
     let kg = event.target.value;
     gramOutput.innerHTML = `${kg * 1000} g`;
+    poundOutput.innerHTML = `${kg * 2.0462262} lb`;
+    ozOutput.innerHTML = `${kg * 35.2739619} oz`;
 });
